a6b2639bb1ef2501769ab44015abebd8bd478df6
Damián Muti <damianmuti@gmail.com>
2016-09-28 16:07:50 -0300

    Cleaned up broken theme and plugins

--- wp-content/themes/lcf/templates/blog-medium/parts/part-content.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** 
- * Content part for blog medium
- * 
- * @package splendid
- */
-?>
-<h2 class="post-title"><a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr(get_the_title()); ?>"><?php the_title(); ?></a></h2>
-<?php echo wpautop(ts_get_the_excerpt_theme(35)); ?>
